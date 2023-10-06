f3fd2a4664ce7ba12e8c0eb96ab13b94bfe97ec8
Mateusz Kosewski <mateusz@sirvoy.com>
2023-10-06 15:54:39 +0200

    Php version

--- script.sh
+++ script.sh
@@ -1,3 +1,11 @@
 #!/usr/bin/env bash
 
-echo "Username: $1";
+version=$(php -v|grep --only-matching --perl-regexp "(PHP )\d+\.\\d+\.\\d+"|cut -c 5-7)
+
+if [ "$version" == '7.2' ]; then
+echo 'Correct'
+else
+echo "Wrong";
+exit 1;
+fi
+
