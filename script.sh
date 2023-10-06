81695d5b3d2a3f1df52a3557fa1c3fedca38eca5
Mateusz Kosewski <mateusz@sirvoy.com>
2023-10-06 15:56:43 +0200

    Phpversion again

--- script.sh
+++ script.sh
@@ -2,10 +2,10 @@
 
 version=$(php -v|grep --only-matching --perl-regexp "(PHP )\d+\.\\d+\.\\d+"|cut -c 5-7)
 
-if [ "$version" == '7.2' ]; then
-echo 'Correct'
+if [ "$version" == '8.2' ]; then
+echo 'Php version is correct'
 else
-echo "Wrong";
+echo "Php version is Wrong";
 exit 1;
 fi
 
