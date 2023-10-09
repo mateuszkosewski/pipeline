ad632e9c13e770971c628ae13f166150eecf3685
Mateusz Kosewski <mateusz@sirvoy.com>
2023-10-09 14:55:13 +0200

    SIO

--- apache.sh
+++ apache.sh
@@ -20,11 +20,11 @@ sudo chown www-data:www-data /home/runner/work
 sudo chown www-data:www-data /home/runner/work/pipeline
 sudo chown www-data:www-data /home/runner/work/pipeline/pipeline
 
-sudo chmod +x /home/
-sudo chmod +x /home/runner/
+#sudo chmod +x /home/
+#sudo chmod +x /home/runner/
 sudo chmod +x /home/runner/work/
-sudo chmod +x /home/runner/work/pipeline/
-sudo chmod +x /home/runner/work/pipeline/pipeline/
+#sudo chmod +x /home/runner/work/pipeline/
+#sudo chmod +x /home/runner/work/pipeline/pipeline/
 
 sudo cat apache.conf | tee -a /etc/apache2/apache2.conf >> /dev/null
 
