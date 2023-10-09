ab02ea56c83641f4c3bcef784a1d9e1ae6cf5af7
Mateusz Kosewski <mateusz@sirvoy.com>
2023-10-09 14:59:20 +0200

    SIO

--- apache.sh
+++ apache.sh
@@ -13,7 +13,7 @@ sudo cat foo.conf | tee -a /etc/apache2/sites-available/000-default.conf >> /dev
 sudo echo "127.0.0.1 pipeline-local.com" | tee -a /etc/hosts >> /dev/null
 
 # Enable apache
-sudo service apache2 start
+#sudo service apache2 start
 
 #sudo adduser www-data root
 #sudo chown www-data:www-data /home/runner
@@ -24,13 +24,13 @@ sudo chmod +x /home/runner/
 
 sudo cat apache.conf | tee -a /etc/apache2/apache2.conf >> /dev/null
 
-sudo service apache2 reload
+sudo service apache2 start
 
 # Open 
-#curl -s -o /dev/null -w "%{http_code}" http://pipeline-local.com
-curl http://pipeline-local.com
+curl -s -o /dev/null -w "%{http_code}" http://pipeline-local.com
+#curl http://pipeline-local.com
 
-cat /var/log/apache2/error.log
+#cat /var/log/apache2/error.log
 #cat /etc/apache2/apache2.conf
 
 #cd /home/runner/work/pipeline/pipeline && ls -l
