e463859210376c8ca78314222e755a8f4b8b5145
Mateusz Kosewski <mateusz@sirvoy.com>
2023-10-09 15:12:52 +0200

    SIO

--- apache.sh
+++ apache.sh
@@ -9,14 +9,6 @@ sudo cat foo.conf | tee -a /etc/apache2/sites-available/000-default.conf >> /dev
 # Add domain to hosts
 sudo echo "127.0.0.1 pipeline-local.com" | tee -a /etc/hosts >> /dev/null
 
-# Enable apache
-#sudo service apache2 start
-
-#sudo adduser www-data root
-#sudo chown www-data:www-data /home/runner
-#sudo chown www-data:www-data /home/runner/work/pipeline
-#sudo chown www-data:www-data /home/runner/work/pipeline/pipeline
-
 sudo chmod +x /home/runner/
 
 sudo cat apache.conf | tee -a /etc/apache2/apache2.conf >> /dev/null
@@ -24,7 +16,7 @@ sudo cat apache.conf | tee -a /etc/apache2/apache2.conf >> /dev/null
 sudo service apache2 start
 
 # Open 
-#curl -s -o /dev/null -w "%{http_code}" http://pipeline-local.com
+curl -s -o /dev/null -w "%{http_code}" http://pipeline-local.com
 curl http://pipeline-local.com
 
 #cat /var/log/apache2/error.log
--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <?php
 
-require __DIR__ . '/vendor/autoload.php';
+// require __DIR__ . '/vendor/autoload.php';
 
-$lipsum = new joshtronic\LoremIpsum();
-print '5 words: ' . $lipsum->words(5);
+// $lipsum = new joshtronic\LoremIpsum();
+// print '5 words: ' . $lipsum->words(5);
+
+print 'Hello world!';
