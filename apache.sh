d3fd274f02739e7307ac2d78dee1451ef49b79b8
Mateusz Kosewski <mateusz@sirvoy.com>
2023-10-09 12:29:04 +0200

    Apache

---
+++ apache.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+cat /etc/apache2/sites-available/000-default.conf
+
+# Overwrite sites-available
+#echo "content to append" >> [file_name]
+
+# Enable apache
+#sudo service apache2 start
+
+# Open 
+ 
+#      - name: Check curl status code
+#        run: 'curl -s -o /dev/null -w "%{http_code}" http://cematic-local.vm/'
