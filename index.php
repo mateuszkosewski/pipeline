ebf4dbd791c068a4e4a8d00d1a93a0a20b4d542d
Mateusz Kosewski <mateusz@sirvoy.com>
2023-10-09 16:17:46 +0200

    DB

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 require __DIR__ . '/vendor/autoload.php';
 
 try {
-    $dbh = new PDO('mysql:host=127.0.0.1;port=3306;dbname=pipeline', 'root', 'root');
+    $dbh = new PDO('mysql:host=localhost;dbname=pipeline', 'root', 'root');
     print 'Connection correct!';
 } catch (PDOException $e) {
     print 'Failed to connect to DB!';
