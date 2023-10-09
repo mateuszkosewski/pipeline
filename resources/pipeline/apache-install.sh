d04e5e6b9ec84a5c5432996b862d3af2b2981064
Mateusz Kosewski <mateusz@sirvoy.com>
2023-10-09 18:56:01 +0200

    Symfony app

--- resources/pipeline/apache-install.sh
+++ resources/pipeline/apache-install.sh
@@ -4,17 +4,17 @@
 sudo apt install libapache2-mod-php8.2
 sudo a2enmod php8.2
 
-# Add domain to sites available
-sudo cat foo.conf | tee -a /etc/apache2/sites-available/000-default.conf >> /dev/null
-
-# Add domain to hosts
-sudo echo "127.0.0.1 pipeline-local.com" | tee -a /etc/hosts >> /dev/null
-
 # Add chmod +x to working directory
 sudo chmod +x /home/runner/
 
 # Add conf to apache
-sudo cat apache.conf | tee -a /etc/apache2/apache2.conf >> /dev/null
+sudo cat resources/pipeline/apache.conf | tee -a /etc/apache2/apache2.conf >> /dev/null
+
+# Add domain to hosts
+sudo echo "127.0.0.1 pipeline-local.com" | tee -a /etc/hosts >> /dev/null
+
+# Add domain to sites available
+sudo cat resources/pipeline/virtual-host.conf | tee -a /etc/apache2/sites-available/000-default.conf >> /dev/null
 
 # Restart apache
 sudo service apache2 start
