e4e07dc6962aee7ee584408e12df0378e07681cc
Mateusz Kosewski <mateusz@sirvoy.com>
2023-10-09 13:27:30 +0200

    Test

--- apache.sh
+++ apache.sh
@@ -1,20 +1,16 @@
 #!/bin/bash
 
-#mkdir /var/www/pipeline
-#mv /home/runner/work/pipeline/pipeline /var/www/pipeline
-
-#ls -l /var/www/pipeline
+sudo cat apache.conf >> /etc/apache2/apache.conf
 
 # Add domain to sites available
-#sudo cat foo.conf | tee -a /etc/apache2/sites-available/000-default.conf >> /dev/null
+sudo cat foo.conf | tee -a /etc/apache2/sites-available/000-default.conf >> /dev/null
 
 # Add domain to hosts
-#sudo echo "127.0.0.1 pipeline-local.com" | tee -a /etc/hosts >> /dev/null
+sudo echo "127.0.0.1 pipeline-local.com" | tee -a /etc/hosts >> /dev/null
 
 # Enable apache
-#sudo service apache2 start
-
-#cat /etc/apache2/apache2.conf
+sudo service apache2 start
 
 # Open 
 #curl -s -o /dev/null -w "%{http_code}" http://pipeline-local.com
+curl http://pipeline-local.com
