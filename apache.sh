6d98aefcbe9a2e616e6fc1fe031b092e2e303c66
Mateusz Kosewski <mateusz@sirvoy.com>
2023-10-09 12:58:12 +0200

    Typo

--- apache.sh
+++ apache.sh
@@ -9,7 +9,7 @@ sudo echo "127.0.0.1 pipeline-local.com" | tee -a /etc/hosts >> /dev/null
 # Enable apache
 sudo service apache2 start
 
-cat /etc/apache2/sites-available/000-default && cat /etc/hosts
+cat /etc/apache2/sites-available/000-default.conf && cat /etc/hosts
 
 # Open 
 curl -s -o /dev/null -w "%{http_code}" http://pipeline-local.com
