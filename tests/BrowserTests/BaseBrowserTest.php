174e2d9fcc19b334296017bcb5f99020eaaefd4b
Mateusz Kosewski <mateusz@sirvoy.com>
2023-10-10 11:57:32 +0200

    Test

--- tests/BrowserTests/BaseBrowserTest.php
+++ tests/BrowserTests/BaseBrowserTest.php
@@ -10,7 +10,7 @@ class BaseBrowserTest extends KernelTestCase {
 	public function testSomething(): void
     {
     	$client = Client::createChromeClient();
-		$crawler = $client->request('GET', 'http://dimatiapi-local.vm');
+		$crawler = $client->request('GET', 'http://pipeline-local.com');
 		$text = $crawler->filter('body')->text();
 
 		self::assertEquals('OK!', $text);
