64e2d9215328931802737d44fe81e7bcdfd66d54
Mateusz Kosewski <mateusz@sirvoy.com>
2023-10-10 12:13:40 +0200

    TEST

--- tests/BrowserTests/BaseBrowserTest.php
+++ tests/BrowserTests/BaseBrowserTest.php
@@ -4,15 +4,20 @@ namespace Tests;
 
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 use Symfony\Component\Panther\Client;
+use Symfony\Component\Panther\DomCrawler\Crawler;
 
 class BaseBrowserTest extends KernelTestCase {
 
+	/*
+  	 * @return void
+     */
 	public function testSomething(): void
     {
     	$client = Client::createChromeClient();
-		$crawler = $client->request('GET', 'http://pipeline-local.com');
-		$text = $crawler->filter('body')->text();
 
-		self::assertEquals('OK!', $text);
+    	//$client->request('GET', 'http://dimatiapi-local.vm');
+		// $text = $crawler->filter('body')->text();
+
+		// self::assertEquals('OK!', $text);
     }
 }
