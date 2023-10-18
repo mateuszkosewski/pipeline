53eaa7adce32eb0663f84bc4872e281ab259e490
Mateusz Kosewski <mateusz@sirvoy.com>
2023-10-18 23:35:38 +0200

    S

--- resources/pipeline/assets-check.sh
+++ resources/pipeline/assets-check.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-UNCOMMITED_CHANGES=$(git --no-pager diff --name-only)
+UNCOMMITED_CHANGES=$(git --no-pager diff --name-only public/build/)
 UNCOMMITED_CHANGES_LENGTH=${#UNCOMMITED_CHANGES}
 
 if (( ${UNCOMMITED_CHANGES_LENGTH} > 0 )); then
-    echo "Assets invalid!"
-    exit 1
+   echo "Assets invalid!"
+   exit 1
 else
-    echo "Assets correct!"
+   echo "Assets correct!"
 fi
